451dd38e841b853604ca472476778ed2f85f9a86
Jason Lowenthal <jason.lowenthal@macmillan.com>
2016-03-02 09:47:08 -0600

    Starting on the navigation definitions css/scripts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,71 @@
-var synapseCss = angular.module('synapseCss',['ngRoute']);
-synapseCss.config(['$routeProvider', function($routeProvider){
-    $routeProvider.when('/navigation',{
+var synapseCss = angular.module('synapseCss', ['ngRoute']);
+synapseCss.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/navigation', {
         templateUrl: 'partials/navigation.html',
-        controller: 'mainController'
+        controller: 'mainController',
     }).otherwise({
         templateUrl: 'partials/home.html',
         controller: 'mainController'
     });
 }]);
 
-synapseCss.controller('mainController', function($scope){
-    $scope.menuItems={
-        navigation:{
+synapseCss.controller('mainController', function ($scope, $route, $routeParams, $location) {
+    console.log($location.path());
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+    $scope.$routes = $route.routes;
+    $scope.$route = $route;
+    $scope.menuItems = {
+        navigation: {
             url: '#/navigation',
             disabled: false,
             text: 'Navigation'
         },
-        menus:{
+        menus: {
             url: '#/menus',
             disabled: true,
             text: 'Menus'
         },
-        tables:{
+        tables: {
             url: '#/tables',
             disabled: true,
             text: 'Tables'
         },
-        widgets:{
+        widgets: {
             url: '#/widgets',
             disabled: true,
             text: 'Widgets'
         },
-
+        search: {
+            url: '#/search',
+            disabled: true,
+            text: 'Search'
+        },
+        forms: {
+            url: '#/forms',
+            disabled: true,
+            text: 'Forms'
+        },
+        modals: {
+            url: '#/modals',
+            disabled: true,
+            text: 'Modals'
+        },
     }
+    availableSubMenuItems = {
+        '/navigation': [{
+            url: '#/navigation#main',
+            disabled: false,
+            text: 'Main Navigation'
+        }, {
+            url: '#/navigation#sub',
+            disabled: false,
+            text: 'Sub Navigation'
+        }]
+    }
+
+    $scope.$on('$routeChangeSuccess', function(){
+        $scope.submenuItems = availableSubMenuItems[$scope.$location.path()];
+    });
 });
+
